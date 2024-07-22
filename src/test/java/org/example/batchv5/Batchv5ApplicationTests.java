0d01e35e59ae7732b4c5b47e00f1495ffddd535d
ha9011 <gkehddnjs90@gmail.com>
2024-07-22 22:20:21 +0900

    batch start

---
+++ src/test/java/org/example/batchv5/Batchv5ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.batchv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Batchv5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
